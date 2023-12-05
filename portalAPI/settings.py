eea8ab1d09b6a834b0e8641184430d70d61bd079
thanhluong <doannguyenthanhluong@gmail.com>
2023-12-06 02:12:23 +0700

    Update settings

--- portalAPI/settings.py
+++ portalAPI/settings.py
@@ -86,6 +86,17 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "istorage_portal.AppUser"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
