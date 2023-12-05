d981169d48021d2ee0484385e55bf920f845174a
thanhluong <doannguyenthanhluong@gmail.com>
2023-12-06 03:03:50 +0700

    Update settings

--- portalAPI/settings.py
+++ portalAPI/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+from .local_settings import *
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
