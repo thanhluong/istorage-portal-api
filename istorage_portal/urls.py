91afcb2c9ccfe7e1aa8cf77a1c7811b5a551fb82
thanhluong <doannguyenthanhluong@gmail.com>
2023-12-06 02:14:24 +0700

    Update codebase structure

--- istorage_portal/urls.py
+++ istorage_portal/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('register', views.UserRegister.as_view(), name='register'),
     path('login', views.UserLogin.as_view(), name='login'),
     path('logout', views.UserLogout.as_view(), name='logout'),
-    path('userinfo', views.User.as_view(), name='user'),
+    path('userinfo', views.UserView.as_view(), name='user'),
 ]
