66a526bfd2472deb2d7beb323879bdcc5bbb9aa3
thanhluong <doannguyenthanhluong@gmail.com>
2023-12-06 03:00:22 +0700

    Update settings

--- portalAPI/settings.py
+++ portalAPI/settings.py
@@ -83,13 +83,6 @@ WSGI_APPLICATION = 'portalAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 AUTH_USER_MODEL = "istorage_portal.AppUser"
 
 REST_FRAMEWORK = {
