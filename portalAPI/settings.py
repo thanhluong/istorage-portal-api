3a7777198718779d10514e43b557a6e23efd03b8
thanhluong <doannguyenthanhluong@gmail.com>
2023-12-06 03:34:36 +0700

    Update settings

--- portalAPI/settings.py
+++ portalAPI/settings.py
@@ -32,6 +32,9 @@ CORS_ALLOWED_ORIGINS = [
     'http://0.0.0.0:8000',
     'http://127.0.0.1:8000',
     'http://localhost:8000',
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'http://localhost'
 ]
 CORS_ALLOWED_CREDENTIALS = True
 
