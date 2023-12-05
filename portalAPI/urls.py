6dd7ed76f376ee6412dcc93ef9b4f0bfbd758483
thanhluong <doannguyenthanhluong@gmail.com>
2023-12-06 03:46:34 +0700

    Update settings

--- portalAPI/urls.py
+++ portalAPI/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/istorage_portal/', include('istorage_portal.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
