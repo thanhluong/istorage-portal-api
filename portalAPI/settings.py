60fc567beab9bc6f47c9b7770f2cb04a6a5591ca
thanhluong <doannguyenthanhluong@gmail.com>
2023-12-06 03:34:09 +0700

    Update settings

--- portalAPI/settings.py
+++ portalAPI/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5z(l1@oj+q@0nt3oh-uton&0ive^u@5kug%vz%#0)e!1@!dqyw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
